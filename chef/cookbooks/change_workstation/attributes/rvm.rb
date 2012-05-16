9f50e572287d336a00ff87881775bbf5b7d57d26
Clay Shentrup <clay@brokenladder.com>
2012-05-15 18:53:21 -0700

    install RVM with Ruby 1.9.3-p0

--- chef/cookbooks/change_workstation/attributes/rvm.rb
+++ chef/cookbooks/change_workstation/attributes/rvm.rb
@@ -1,4 +1,8 @@
 include_attribute("pivotal_workstation::rvm")
 
-node.override["rvm"]["rubies"] = ["ruby-1.9.3-p0"]
-node.override["rvm"]["default_ruby"] = "ruby-1.9.3-p0"
+node[:rvm][:rubies].delete('ruby-1.9.3-p194') # don't waste time installing Pivotal's version
+
+node.normal[:rvm] = {
+  :rubies => {'ruby-1.9.3-p0' => { :command_line_options => '--with-gcc=clang' }},
+  :default_ruby => 'ruby-1.9.3-p0'
+}
