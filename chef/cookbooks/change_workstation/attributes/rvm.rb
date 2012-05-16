fcd4b3868d8bdd7f784533e1d6e6a1ffa38a8c35
Clay Shentrup <clay@brokenladder.com>
2012-05-16 09:49:49 -0700

    Using Ruby 1.9.3-p194 instead of -p0 because it compiles with the OSX Command Line Tools for Xcode (last time we were actually using p125)

--- chef/cookbooks/change_workstation/attributes/rvm.rb
+++ chef/cookbooks/change_workstation/attributes/rvm.rb
@@ -1,9 +1,8 @@
-# Uncomment this code if you ever need to use an RVM Ruby version other than the default in pivotal_workstation
-# include_attribute("pivotal_workstation::rvm")
-# 
-# node[:rvm][:rubies].delete('ruby-1.9.3-p194') # don't waste time installing Pivotal's version
-# 
-# node.normal[:rvm] = {
-#   :rubies => {'ruby-1.9.3-p0' => { :command_line_options => '--with-gcc=clang' }},
-#   :default_ruby => 'ruby-1.9.3-p0'
-# }
+include_attribute("pivotal_workstation::rvm")
+
+node[:rvm][:rubies].delete('ruby-1.9.3-p125') # don't waste time installing Pivotal's version
+
+node.normal[:rvm] = {
+  :rubies => {'ruby-1.9.3-p194' => { :command_line_options => '--with-gcc=clang' }},
+  :default_ruby => 'ruby-1.9.3-p194'
+}
