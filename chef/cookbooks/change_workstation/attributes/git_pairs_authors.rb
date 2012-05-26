f36abbfc8e4da35187d83fa52054ed137f059144
Clay Shentrup & Eric Nicholas <pair+clay+enicholas@pivotallabs.com>
2012-05-26 13:53:04 -0700

    add git pairs, git_config_global_defaults

---
+++ chef/cookbooks/change_workstation/attributes/git_pairs_authors.rb
@@ -0,0 +1,18 @@
+node.default["git_pairs_authors"] = [
+  {"initials" => "ts", "name" => "Thomas Shafer", "shortname" => "thomas"},
+  {"initials" => "md", "name" => "Mark Dimas", "shortname" => "mark"},
+  {"initials" => "cf", "name" => "Charles Finkel", "shortname" => "charles"},
+  {"initials" => "rc", "name" => "Rex Chung", "shortname" => "rex"},
+  {"initials" => "cvs", "name" => "Clay Shentrup", "shortname" => "clay"},
+  {"initials" => "ab", "name" => "Alain Bloch", "shortname" => "alain"},
+  {"initials" => "eo", "name" => "Erik Ogan", "shortname" => "erik"},
+  {"initials" => "jg", "name" => "Jared Grippe", "shortname" => "jared"},
+  {"initials" => "mp", "name" => "Michael Pabst", "shortname" => "mpabst"},
+  {"initials" => "kv", "name" => "Kyle VanderBeek", "shortname" => "kyle"},
+  {"initials" => "en", "name" => "Eric Nicholas", "shortname" => "enicholas"},
+  {"initials" => "es", "name" => "Ed Shadi", "shortname" => "ed"},
+  {"initials" => "cs", "name" => "Chase Stubblefield", "shortname" => "chase"},
+  {"initials" => "af", "name" => "Ali Faiz", "shortname" => "ali"},
+  {"initials" => "el", "name" => "Eric Lukoff", "shortname" => "elukoff"},
+  {"initials" => "vr", "name" => "Vijay Krishna Ramesh", "shortname" => "vijay"}
+  ]
